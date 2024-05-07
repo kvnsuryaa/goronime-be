d7fd750387a26972614bb5d527d1fe99d6b59dcc
trickas <trickas91@gmail.com>
2024-05-07 07:43:10 +0700

    update: access location

--- src/master/studio/studio.controller.ts
+++ src/master/studio/studio.controller.ts
@@ -4,12 +4,12 @@ import { CreateStudioDto } from './dto/create-studio.dto';
 import { UpdateStudioDto } from './dto/update-studio.dto';
 import { AuthGuard } from '@nestjs/passport';
 
-@UseGuards(AuthGuard('jwt'))
 @Controller('studio')
 export class StudioController {
   constructor(private readonly studioService: StudioService) { }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   create(@Body() createStudioDto: CreateStudioDto) {
     return this.studioService.create(createStudioDto);
   }
@@ -20,16 +20,19 @@ export class StudioController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   findOne(@Param('id') id: string) {
     return this.studioService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
   update(@Param('id') id: string, @Body() updateStudioDto: UpdateStudioDto) {
     return this.studioService.update(id, updateStudioDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   remove(@Param('id') id: string) {
     return this.studioService.remove(id);
   }
